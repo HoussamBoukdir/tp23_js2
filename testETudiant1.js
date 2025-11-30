152b287df9285739d2628efd48dd470c5a54cbe9
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-11-30 16:24:15 +0100

    Add files via upload

---
+++ testETudiant1.js
@@ -0,0 +1,9 @@
+import Etudiant1, {Etablissement as ets, infoEtudiant} from "./Etudiant1.js";
+//import Etudiant1, {Etablissement} from "./Etudiant1.js";
+ 
+console.log(ets);
+//console.log(Etablissement);
+ 
+let e1=new Etudiant1("Nabil", 25);
+ 
+console.log(infoEtudiant(e1));
